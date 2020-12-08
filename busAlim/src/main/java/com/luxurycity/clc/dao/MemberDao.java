eba546fb4008bec5f8a2f13041e43cb0914f96dc
dolph <dolph@DESKTOP-V56LA6H.mshome.net>
2020-12-08 16:34:21 +0900

    day1208 1st commit(로그인, 로그아웃)

--- busAlim/src/main/java/com/luxurycity/clc/dao/MemberDao.java
+++ busAlim/src/main/java/com/luxurycity/clc/dao/MemberDao.java
@@ -1,5 +1,21 @@
 package com.luxurycity.clc.dao;
 
-public class MemberDao {
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 
+import com.luxurycity.clc.vo.*;
+
+public class MemberDao {
+	@Autowired
+	SqlSessionTemplate sqlSession;
+	
+	// 로그인 검사 전담 처리 함수(영선)
+	public int loginCnt(MemberVO mVO) {
+		return sqlSession.selectOne("mSQL.loginCnt", mVO);
+	}
+	
+	// 메인에 표시되는 아바타 데이터 가져오기 전담 처리 함수(영선)
+	public AvatarVO getAvt(String id) {
+		return sqlSession.selectOne("mSQL.getMembAvt", id);
+	}
 }
