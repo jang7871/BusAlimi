f5def49ded3eadb02ae83cecfee7da6d326dcb81
ckdn9 <ckdn9@DESKTOP-LKNLQ9E>
2020-12-15 12:45:53 +0900

    12.15 12:45 sun + jiwoo branch merge

--- busAlim/src/main/java/com/luxurycity/clc/service/MemberService.java
+++ busAlim/src/main/java/com/luxurycity/clc/service/MemberService.java
@@ -1,6 +1,8 @@
 package com.luxurycity.clc.service;
 
 
+import java.util.HashMap;
+
 import javax.servlet.http.*;
 
 import org.springframework.beans.factory.annotation.*;
@@ -13,7 +15,8 @@ public class MemberService {
 
 		@Autowired
 		MemberDao mDao;
-		
+		@Autowired
+		BookmarkDao bmDao;
 		// 회원정보 가져오는 함수
 		public void setMyInfo(ModelAndView mv, MemberVO mVO, AvatarVO aVO, HttpSession session) {
 			String id = (String) session.getAttribute("SID");
@@ -42,6 +45,39 @@ public class MemberService {
 			System.out.println("확인용 : " + mVO.getAno());
 			
 			mv.setViewName("redirect:/member/mypage.clc");
+		}
+		
+		
+		public HashMap<String, String> setBookAdd(HashMap<String, Object> map, HttpSession session, BookmarkVO bmVO) {
+			HashMap<String, String> map2 = new HashMap<String, String>();
+			String result = "";
+			// 1. 파라미터 값을 꺼내온다.
+			String type = (String) map.get("type");
+			// 2. 세션 검사
+			String id = (String) session.getAttribute("SID");
+			if(id == null) {
+				result = "LOGIN";
+				map2.put("result", result);
+				return map2;
+			}
+			map.put("id", id);
+			// 3. dao 처리
+			int cnt = 0;
+			cnt = bmDao.bookAdd(map);
+			if(cnt == 0) {
+				result = "NO";
+			} else {
+				if(type.equals("bus")) {
+					result = "BUSADD";
+				} else if(type.equals("station")) {
+					result = "STAADD";
+				} else if(type.equals("busstation")) {
+					result = "BUSSTAADD";
+				}
+			}
+			map2.put("result", result);
+			
+			return map2;
 		}
 
 }
