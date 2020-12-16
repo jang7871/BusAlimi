dbbe751d3b1a22c09da07ac15a1f9529d64cc385
dolph <dolph@DESKTOP-V56LA6H.mshome.net>
2020-12-16 10:51:38 +0900

    12.16 영선 1st commit(즐찾 삭제 버그 수정)

--- busAlim/src/main/webapp/resources/js/mypage.js
+++ busAlim/src/main/webapp/resources/js/mypage.js
@@ -62,46 +62,50 @@ $(document).ready(function(){
 	
 	// 정류소 버튼 클릭 시 정류소 상세검색 페이지로 이동
 	$('.sBtn').click(function(){
-		var station_id = $(this).children().first().attr('id');
-		let sfrm = $(document.createElement('form'));
-		$(sfrm).attr('method', 'POST');
-		$(sfrm).attr('action', '/clc/search/stationdetail.clc');
-		
-		let sinput = $(document.createElement('input'));
-		$(sinput).attr('type', 'hidden');
-		$(sinput).attr('name', 'station_id');
-		$(sinput).val(station_id);
-		
-		$(sfrm).append(sinput);
-		$('body').prepend(sfrm);
-		
-		$(sfrm).submit();
-	});
-	
-	// 버스 버튼 클릭 시 버스 상세검색 페이지로 이동
-	$('.rBtn').click(function(){
-		var route_id = $(this).children().first().attr('id');
-		let rfrm = $(document.createElement('form'));
-		$(rfrm).attr('method', 'GET');
-		$(rfrm).attr('action', '/clc/search/busdetail.clc');
-		
-		let rinput = $(document.createElement('input'));
-		$(rinput).attr('type', 'hidden');
-		$(rinput).attr('name', 'route_id');
-		$(rinput).val(route_id);
-		
-		var station_id = $(this).children().children().first().attr('id');
-		if(station_id != 0) {
+		if($('#deletebtn').hasClass('w3-hide')) {
+			var station_id = $(this).children().first().attr('id');
+			let sfrm = $(document.createElement('form'));
+			$(sfrm).attr('method', 'POST');
+			$(sfrm).attr('action', '/clc/search/stationdetail.clc');
+			
 			let sinput = $(document.createElement('input'));
 			$(sinput).attr('type', 'hidden');
 			$(sinput).attr('name', 'station_id');
 			$(sinput).val(station_id);
-			$(rfrm).append(sinput);
+			
+			$(sfrm).append(sinput);
+			$('body').prepend(sfrm);
+			
+			$(sfrm).submit();
+		} 
+	});
+	
+	// 버스 버튼 클릭 시 버스 상세검색 페이지로 이동
+	$('.rBtn').click(function(){
+		if($('#deletebtn').hasClass('w3-hide')) {
+			var route_id = $(this).children().first().attr('id');
+			let rfrm = $(document.createElement('form'));
+			$(rfrm).attr('method', 'GET');
+			$(rfrm).attr('action', '/clc/search/busdetail.clc');
+			
+			let rinput = $(document.createElement('input'));
+			$(rinput).attr('type', 'hidden');
+			$(rinput).attr('name', 'route_id');
+			$(rinput).val(route_id);
+			
+			var station_id = $(this).children().children().first().attr('id');
+			if(station_id != 0) {
+				let sinput = $(document.createElement('input'));
+				$(sinput).attr('type', 'hidden');
+				$(sinput).attr('name', 'station_id');
+				$(sinput).val(station_id);
+				$(rfrm).append(sinput);
+			}
+			
+			$(rfrm).append(rinput);
+			$('body').prepend(rfrm);
+			
+			$(rfrm).submit();
 		}
-		
-		$(rfrm).append(rinput);
-		$('body').prepend(rfrm);
-		
-		$(rfrm).submit();
 	});
 });
